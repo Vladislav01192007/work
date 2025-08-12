b3e6e43a65f1667489096f01b707c3f02f1b31ff
ALTSETING <vladislav.melnyk.kripto@gmail.com>
2025-08-12 21:45:15 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,5 @@
 const themeSelect = document.getElementById("themeSelect");
 
 themeSelect.addEventListener("change", () => {
-  const body = document.body;
-  switch (themeSelect.value) {
-    case "grayblack":
-      body.style.background = "linear-gradient(135deg, #2e2e2e, #0b0b0b)";
-      break;
-    case "bluepurple":
-      body.style.background = "linear-gradient(135deg, #1e3c72, #2a0845)";
-      break;
-    case "greenblack":
-      body.style.background = "linear-gradient(135deg, #0f9b0f, #000000)";
-      break;
-    case "sunset":
-      body.style.background = "linear-gradient(135deg, #ff7e5f, #feb47b)";
-      break;
-  }
+  document.body.style.background = "linear-gradient(135deg, #2e2e2e, #0b0b0b)";
 });
